a89e3ce108a7bc827b90844271c3f6fdcf950ba4
vitoriaemilly2007 <160539668+vitoriaemilly2007@users.noreply.github.com>
2024-09-17 14:37:22 -0300

    Update instrutor.py

--- instrutor.py
+++ instrutor.py
@@ -1,8 +1,37 @@
 class instrutor:
-    def __init__(self, Nome, matricula, cpf):
+    def __init__(self, Nome, matricula, especialidade):
         self.nome = Nome
         self.matricula = matricula
-        self.cpf = cpf
+        self.especialidade = especialidade
+
+   def get_nome(self):
+       return self.name
+       
+   def set_nome(self,nome):
+       self.__nome = nome
+
+   def get_matricula(self):
+       return self.matricula
+
+   def set_nome(self,matricula):
+       self.__matricula = matricula
+
+   def get_especialidade(self):
+       return self.especialidade
+
+   def set_nome(self,especialidade):
+       self.__especialidade = especialidade
+
+   def criar_treino(self, aluno, planoDeTreino):
+       print(f'O instrutor {self.__nome} criou um treino {treino.get_nome()} para o aluno {aluno.get_nome()}.)
+    
+   
+
+   
+
+   
+     
+
 
         
 
